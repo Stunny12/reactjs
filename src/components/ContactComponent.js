3d85a0d262dcdce57b9679953d8cd10fd6e8581a
Ragupathi Devaraj <ragumousy@gmail.com>
2022-08-04 20:56:33 +0530

    Redux Thunk End

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -127,7 +127,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                       <h3>Send us your Feedback</h3>
                    </div>
                     <div className="col-12 col-md-9">
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <LocalForm model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                     <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
